ca543409f9e30bcb87b7d0039618883b16d8649f
gbelda <gbelda56@gmail.com>
2021-11-11 15:20:41 +0100

    Created SiteMain component and imported in main app vue file

--- src/App.vue
+++ src/App.vue
@@ -2,65 +2,7 @@
   <div id="app">
     <SiteHeader />
     <!-- /#site_header -->
-    <main id="site_main">
-      <section class="content_section container">
-        <h2>-- Content goes here --</h2>
-      </section>
-      <!-- /.content_section -->
-      <section class="content_index">
-        <ul class="index_options d-flex container">
-          <li>
-            <a href="#">
-              <div class="index_link">
-                <img src="./assets/img/buy-comics-digital-comics.png" alt="" />
-                <h5>DIGITAL COMICS</h5>
-              </div>
-            </a>
-          </li>
-          <li>
-            <a href="#">
-              <div class="index_link">
-                <img src="./assets/img/buy-comics-merchandise.png" alt="" />
-
-                <h5>DC MERCHANDISE</h5>
-              </div>
-            </a>
-          </li>
-          <li>
-            <a href="#">
-              <div class="index_link">
-                <img src="./assets/img/buy-comics-subscriptions.png" alt="" />
-
-                <h5>SUBSCRIPTION</h5>
-              </div>
-            </a>
-          </li>
-          <li>
-            <a href="#">
-              <div class="index_link">
-                <img
-                  src="./assets/img/buy-comics-shop-locator.png"
-                  alt=""
-                  class="locator_logo"
-                />
-
-                <h5>COMIC SHOP LOCATOR</h5>
-              </div>
-            </a>
-          </li>
-          <li>
-            <a href="#">
-              <div class="index_link">
-                <img src="./assets/img/buy-dc-power-visa.svg" alt="" />
-
-                <h5>DC POWER VISA</h5>
-              </div>
-            </a>
-          </li>
-        </ul>
-      </section>
-      <!-- /.content_index -->
-    </main>
+    <SiteMain />
     <!-- /#site_main -->
     <footer id="site_footer">
       <section class="ext_links"></section>
@@ -74,10 +16,12 @@
 
 <script>
 import SiteHeader from "./components/SiteHeader.vue";
+import SiteMain from "./components/SiteMain.vue";
 export default {
   name: "App",
   components: {
     SiteHeader,
+    SiteMain,
   },
 };
 </script>
@@ -85,41 +29,4 @@ export default {
 <style lang="scss">
 @import "./assets/scss/common.scss";
 @import "./assets/scss/variables.scss";
-#site_main {
-  background-color: $content_bg_color;
-
-  .content_section {
-    color: $text_primary_color;
-    padding: 3rem 0px;
-  }
-
-  .content_index {
-    background-color: $highlight_primary_color;
-    padding: 3rem 0px;
-
-    .index_options {
-      justify-content: space-around;
-      align-items: center;
-
-      h5 {
-        color: $text_primary_color;
-        font-weight: normal;
-        padding-left: 10px;
-      }
-    }
-
-    .index_link {
-      display: flex;
-      align-items: center;
-    }
-
-    .locator_logo {
-      width: 30px;
-    }
-
-    img {
-      width: 40px;
-    }
-  }
-}
 </style>
---
+++ src/components/SiteMain.vue
@@ -0,0 +1,106 @@
+<template>
+  <main id="site_main">
+    <section class="content_section container">
+      <h2>-- Content goes here --</h2>
+    </section>
+    <!-- /.content_section -->
+    <section class="content_index">
+      <ul class="index_options d-flex container">
+        <li>
+          <a href="#">
+            <div class="index_link">
+              <img src="../assets/img/buy-comics-digital-comics.png" alt="" />
+              <h5>DIGITAL COMICS</h5>
+            </div>
+          </a>
+        </li>
+        <li>
+          <a href="#">
+            <div class="index_link">
+              <img src="../assets/img/buy-comics-merchandise.png" alt="" />
+
+              <h5>DC MERCHANDISE</h5>
+            </div>
+          </a>
+        </li>
+        <li>
+          <a href="#">
+            <div class="index_link">
+              <img src="../assets/img/buy-comics-subscriptions.png" alt="" />
+
+              <h5>SUBSCRIPTION</h5>
+            </div>
+          </a>
+        </li>
+        <li>
+          <a href="#">
+            <div class="index_link">
+              <img
+                src="../assets/img/buy-comics-shop-locator.png"
+                alt=""
+                class="locator_logo"
+              />
+
+              <h5>COMIC SHOP LOCATOR</h5>
+            </div>
+          </a>
+        </li>
+        <li>
+          <a href="#">
+            <div class="index_link">
+              <img src="../assets/img/buy-dc-power-visa.svg" alt="" />
+
+              <h5>DC POWER VISA</h5>
+            </div>
+          </a>
+        </li>
+      </ul>
+    </section>
+    <!-- /.content_index -->
+  </main>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss">
+@import "../assets/scss/variables.scss";
+#site_main {
+  background-color: $content_bg_color;
+
+  .content_section {
+    color: $text_primary_color;
+    padding: 3rem 0px;
+  }
+
+  .content_index {
+    background-color: $highlight_primary_color;
+    padding: 3rem 0px;
+
+    .index_options {
+      justify-content: space-around;
+      align-items: center;
+
+      h5 {
+        color: $text_primary_color;
+        font-weight: normal;
+        padding-left: 10px;
+      }
+    }
+
+    .index_link {
+      display: flex;
+      align-items: center;
+    }
+
+    .locator_logo {
+      width: 30px;
+    }
+
+    img {
+      width: 40px;
+    }
+  }
+}
+</style>
