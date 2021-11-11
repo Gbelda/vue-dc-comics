2723783a43f9af855d018745a0e096eff7b7912a
gbelda <gbelda56@gmail.com>
2021-11-11 13:47:25 +0100

    created app vue html template structure

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,24 @@
 <template>
-  <div id="app"></div>
+  <div id="app">
+    <header id="site_header">
+      <nav class="nav_bar"></nav>
+    </header>
+    <!-- /#site_header -->
+    <main id="site_main">
+      <section class="content_section"></section>
+      <!-- /.content_section -->
+      <section class="content_index"></section>
+      <!-- /.content_index -->
+    </main>
+    <!-- /#site_main -->
+    <footer id="site_footer">
+      <section class="ext_links"></section>
+      <!-- /.ext_links -->
+      <section class="socials"></section>
+      <!-- /.socials -->
+    </footer>
+    <!-- /#site_footer -->
+  </div>
 </template>
 
 <script>
